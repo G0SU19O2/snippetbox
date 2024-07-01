f0525e2da87127d075deaf6d6246cc0781e725ce
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-07-01 18:30:40 +0700

    User authentication

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -16,11 +18,17 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash: app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/justinas/nosurf v1.1.1
+	golang.org/x/crypto v0.24.0
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/alexedwards/scs/mysqlstore v0.0.0-20240316134038-7e11d57e8885
 	github.com/alexedwards/scs/v2 v2.8.0
